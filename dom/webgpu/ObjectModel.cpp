fb6e440992df21d23dcfcc7c4b0c36919150fce2
Erich Gubler <erichdongubler@gmail.com>
2023-06-01 16:37:26 -0400

    WIP: Bug ???????: rewrite and doc. `ObjectModel` APIs for clarity r=#webgpu-reviewers

--- dom/webgpu/ObjectModel.cpp
+++ dom/webgpu/ObjectModel.cpp
@@ -3,39 +3,25 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "ObjectModel.h"
+// #include "ObjectModel.h"
 
-#include "Adapter.h"
-#include "ShaderModule.h"
-#include "CompilationInfo.h"
-#include "Device.h"
-#include "CommandEncoder.h"
-#include "Instance.h"
-#include "Texture.h"
-#include "nsIGlobalObject.h"
+// #include "Adapter.h"
+// #include "ShaderModule.h"
+// #include "CompilationInfo.h"
+// #include "Device.h"
+// #include "CommandEncoder.h"
+// #include "Instance.h"
+// #include "Texture.h"
+// #include "nsIGlobalObject.h"
 
 namespace mozilla::webgpu {
 
-template <typename T>
-ChildOf<T>::ChildOf(T* const parent) : mParent(parent) {}
-
-template <typename T>
-ChildOf<T>::~ChildOf() = default;
-
-template <typename T>
-nsIGlobalObject* ChildOf<T>::GetParentObject() const {
-  return mParent->GetParentObject();
-}
-
-void ObjectBase::GetLabel(nsAString& aValue) const { aValue = mLabel; }
-void ObjectBase::SetLabel(const nsAString& aLabel) { mLabel = aLabel; }
-
-template class ChildOf<Adapter>;
-template class ChildOf<ShaderModule>;
-template class ChildOf<CompilationInfo>;
-template class ChildOf<CommandEncoder>;
-template class ChildOf<Device>;
-template class ChildOf<Instance>;
-template class ChildOf<Texture>;
+// template class HasParentObject<Adapter>;
+// template class HasParentObject<ShaderModule>;
+// template class HasParentObject<CompilationInfo>;
+// template class HasParentObject<CommandEncoder>;
+// template class HasParentObject<Device>;
+// template class HasParentObject<Instance>;
+// template class HasParentObject<Texture>;
 
 }  // namespace mozilla::webgpu
