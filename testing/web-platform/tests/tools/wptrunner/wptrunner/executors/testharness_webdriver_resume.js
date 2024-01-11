efd1355a454a56d29162373b996d013713a12ee9
Erich Gubler <erichdongubler@gmail.com>
2024-01-10 22:34:22 -0500

    WIP: Bug ???????: test: s/`/"/g in `testharness_webdriver_resume.js` to accept query params with double quotes
    We on the WebGPU team want to start including test variants in WebGPU
    CTS that contain more specific parameters for tests. Their current
    design is to use double-quotes for string test parameters, which breaks
    this template's interpolation of the query parameters into a
    double-quoted string literal.
    
    TODO: Might be totally invalid after rebasing onto new changes,
    double-check plz!

--- testing/web-platform/tests/tools/wptrunner/wptrunner/executors/testharness_webdriver_resume.js
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/executors/testharness_webdriver_resume.js
@@ -1,5 +1,5 @@
 // We have to set the url here to ensure we get the same escaping as in the harness
 // and also to handle the case where the test changes the fragment
-window.__wptrunner_url = arguments[0];
+window.__wptrunner_url = `%(url)s`; // TODO: is there something better to do here? NOTE: This has changed since initial authoring, see if this isn't sufficient now.
 window.__wptrunner_testdriver_callback = arguments[arguments.length - 1];
 window.__wptrunner_process_next_event();
