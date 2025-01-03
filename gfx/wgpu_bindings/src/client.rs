95f9f3399d8ae8be6f068bf729b2b03ea3b4772d
Erich Gubler <erichdongubler@gmail.com>
2025-01-02 21:33:24 -0500

    WIP: Bug 1939787 - build(webgpu): update WGPU to 5543961a71cc8e5399b696fae3f6aae82c019717 r=#webgpu-reviewers!
    Also remove the now-unused shim adapting from `cfg_aliases` 0.1 to 0.2.

--- gfx/wgpu_bindings/src/client.rs
+++ gfx/wgpu_bindings/src/client.rs
@@ -549,6 +549,7 @@ pub extern "C" fn wgpu_client_create_texture_view(
             base_array_layer: desc.base_array_layer,
             array_layer_count: desc.array_layer_count.map(|ptr| *ptr),
         },
+        usage: None,
     };
 
     let action = TextureAction::CreateView(id, wgpu_desc);
