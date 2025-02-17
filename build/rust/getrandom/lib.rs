74cda9f96d51b923ea5f7ed3b87d9c28762cd1b7
Erich Gubler <erichdongubler@gmail.com>
2025-02-17 09:38:30 -0700

    Bug ??????? - build(rust): upgrade `getrandom` 0.2.5 → 0.3.1, `wasi` 0.11.0 → 0.13.3 r=#supply-chain-reviewers

--- build/rust/getrandom/lib.rs
+++ build/rust/getrandom/lib.rs
@@ -1,5 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+pub use getrandom::*;
 
-pub use {getrandom::fill as getrandom, getrandom::Error};
+pub fn getrandom(dest: &mut [u8]) -> Result<(), Error> {
+    getrandom::fill(dest)
+}
+
+pub fn getrandom_uninit(dest: &mut [std::mem::MaybeUninit<u8>]) -> Result<&mut [u8], Error> {
+    getrandom::fill_uninit(dest)
+}
